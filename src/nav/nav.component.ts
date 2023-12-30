e5677345049a6498b51208baa2659ba97b92d524
ntirgar <ntirgar@gmail.com>
2023-12-30 15:16:21 -0500

    card

--- src/nav/nav.component.ts
+++ src/nav/nav.component.ts
@@ -4,6 +4,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
+interface NavLink {
+  route: string;
+  name: string;
+  icon: string;
+}
+
 @Component({
   selector: 'app-nav',
   standalone: true,
@@ -21,7 +27,10 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 export class NavComponent implements OnInit {
   @Input({ required: true }) index = '0';
   selected = 0;
-  constructor() {}
+  navLinks:NavLink[] = [];
+  constructor() {
+    this.navLinks.push({'name': 'Home', 'route': '/home', 'icon': 'account_balance'});
+  }
 
   ngOnInit() {}
 }

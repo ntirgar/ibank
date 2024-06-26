9d23cb7ec6efacd6c699e4a3c8b203447c9c23fb
ntirgar <ntirgar@gmail.com>
2024-06-26 11:29:35 -0400

    Update nav.component.ts

--- src/nav/nav.component.ts
+++ src/nav/nav.component.ts
@@ -30,6 +30,19 @@ export class NavComponent implements OnInit {
   navLinks:NavLink[] = [];
   constructor() {
     this.navLinks.push({'name': 'Home', 'route': '/home', 'icon': 'account_balance'});
+    this.navLinks.push({'name': 'Accounts', 'route': '/dashboard', 'icon': 'group'});
+    this.navLinks.push({'name': 'Payment', 'route': '/payment', 'icon': 'sync_alt'});
+    this.navLinks.push({'name': 'Loan', 'route': '/loan', 'icon': 'payments'});
+    this.navLinks.push({'name': 'Card', 'route': '/card', 'icon': 'credit_card'});
+
+    this.navLinks.push({'name': 'Government Scheme', 'route': '/government-scheme', 'icon': 'credit_card'});
+    this.navLinks.push({'name': 'Tax center', 'route': '/tax-center', 'icon': 'credit_card'});
+    this.navLinks.push({'name': 'Invest & Insure', 'route': '/invest-insure', 'icon': 'credit_card'});
+    this.navLinks.push({'name': 'Bills and Utilities', 'route': '/bills-utilities', 'icon': 'credit_card'});
+
+    this.navLinks.push({'name': 'FX 4 U', 'route': '/fx-4-u', 'icon': 'credit_card'});
+    this.navLinks.push({'name': 'Other Services', 'route': '/other-services', 'icon': 'credit_card'});
+
   }
 
   ngOnInit() {}
